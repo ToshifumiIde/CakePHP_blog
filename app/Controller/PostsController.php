3195b831795a58c354fbb84033dd6a0a789ddd10
ToshifumiIde <toshifumiide198765@gmail.com>
2021-05-10 10:08:31 +0900

    create Post_View_index.ctp

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,9 +1,10 @@
 <?php
 
 class PostsController extends AppController{
-  //デフォルトで用意されているhelpersメソッドの使用
+  //各コントローラーにデフォルトで用意されている$helpersプロパティを用意。プロパティ内にはViewで使用できるヘルパーの一覧が保持されている。
+  //Viewでhelperを使用するには、helperの名前をコントローラーの$helpers配列に追加する。
   public $helpers = array("Html" , "Form");
-  
+
   //posts/indexへのアクセスに必要なメソッド
   public function index(){
     //set()メソッドで変数を用意
