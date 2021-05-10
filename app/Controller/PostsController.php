e5aa55989a3fb72a99713ac7470d44ce0bb94eda
ToshifumiIde <toshifumiide198765@gmail.com>
2021-05-10 10:32:23 +0900

    add view method in PostsController

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -8,6 +8,21 @@ class PostsController extends AppController{
   //posts/indexへのアクセスに必要なメソッド
   public function index(){
     //set()メソッドで変数を用意
+    //"posts"に対し「（自分で作成した）Postモデル」のfind()メソッドを用いて"all"を指定することで、dbの全ての情報を取得する。
     $this->set("posts" , $this->Post->find("all"));
+  }
+
+  //posts/viewへのアクセスに必要なメソッド
+  //index.ctpにて"action"=>"view"を設定しているため、view()メソッド
+  public function view($id = null){
+    if(!$id){
+      throw new NotFoundException(__("Invalid post"));
+    }
+    //（自分で作成した）PostモデルのfindById()メソッドを用い、find()メソッドとは異なり、Idでの取得を実施
+    $post = $this->Post->findById($id);
+    if(!$post){
+      throw new NotFoundException(__("Invalid post"));
+    }
+    $this->set("post" , $post);
   }
 }
