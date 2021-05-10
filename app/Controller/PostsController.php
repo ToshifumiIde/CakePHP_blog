617835d58a6f97cc8f53bb47e5e79c4406cba4d6
ToshifumiIde <toshifumiide198765@gmail.com>
2021-05-10 13:32:21 +0900

    add edit method in PostsController

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -40,4 +40,29 @@ class PostsController extends AppController{
       $this->Flash->error(__("Unable to add your post."));
     }
   }
+  public function edit($id = null){
+    //idが存在するか確認
+    if(!$id){
+      throw new NotFoundException(__("Invalid post"));
+    }
+    
+    //リクエストがPOSTかPUTか確認
+    $post = $this->Post->FindById($id);
+    if(!$post){
+      throw new NotFoundException(__("Invalid post"));
+    }
+
+    if($this->request->is(array("post","put"))){
+      $this->Post->id = $id;
+      if($this->Post->save($this->request->data)){
+        $this->Flush->success(("Your post has been updated"));
+        return $this->redirect(array("action" => "index"));
+      }
+      $this->Flash->error(__("Unable to update your post."));
+    }
+
+    if(!$this->request->data){
+      $this->request->data = $post;
+    }
+  }
 }
