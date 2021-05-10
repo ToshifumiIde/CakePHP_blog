d5302ac2b7f97106c9a94742367ebab6a286b962
ToshifumiIde <toshifumiide198765@gmail.com>
2021-05-10 11:48:33 +0900

    add validation property in Model/Post.php

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,5 +1,14 @@
 <?php
 
-class Post extends AppModel{
-  
+class Post extends AppModel {
+  //$validate配列を用いてsave()メソッドが呼ばれた際、どのようにvalidateするかCakePHPに教える。
+  //ここでは、titleとbodyが空ではNGという設定を実施。
+  public $validate = array(
+    "title" => array(
+      "rule" => "notBlank",
+    ),
+    "body" =>array(
+      "rule" => "notBlank",
+    ),
+  );
 }
